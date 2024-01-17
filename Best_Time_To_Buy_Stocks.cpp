783b5c1d0176ad6906679a9ddb3e0746822cdbdf
Shubh Mittal <59731205+shubhmittal07@users.noreply.github.com>
2024-01-17 09:27:29 -0800

    Update Best_Time_To_Buy_Stocks.cpp

--- Best_Time_To_Buy_Stocks.cpp
+++ Best_Time_To_Buy_Stocks.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_pro  = 0;
+        int n = prices.size();
+        int l=0,r=1;
+        while(r<n){
+            if(prices[l] < prices[r]){
+                int temp = prices[r] - prices[l];
+                max_pro = max(max_pro,temp);
+            }
+            else l = r;
+            r++;
+        }
+        return max_pro;
+    }
+};
